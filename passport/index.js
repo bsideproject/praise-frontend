dc7c88a445659ccdf601af583828452eacecebdd
ekrte <ekrte@github.com>
2022-06-12 18:50:49 +0900

    Add: 카카오 소셜 로그인 기능

---
+++ passport/index.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const kakao = require('./kakaoStrategy');
+ 
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+
+    kakao();
+};
