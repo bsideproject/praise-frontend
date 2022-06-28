07844a28881e06cf05b189f1c91835214a82cd88
toycrane <alwaysfun2183@gmail.com>
2022-06-28 18:50:08 +0900

    feature: Footer에 Route 추가

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Alarm, CalendarCheck, Smiley } from "phosphor-react";
 import styled from "styled-components";
 
@@ -23,6 +24,7 @@ const NavigationItem = styled.li`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
+	cursor: pointer;
 `;
 
 const NavigationText = styled.div`
@@ -30,22 +32,39 @@ const NavigationText = styled.div`
 	line-height: 150%;
 `;
 
+const NavLinks = [
+	{
+		name: "DAILY_MISSION",
+		path: "/daily-missions",
+		text: "데일리 미션",
+		icon: <Alarm size={32} />,
+	},
+	{
+		name: "MISSION_STATUS",
+		path: "/mission-status",
+		text: "미션 현황",
+		icon: <CalendarCheck size={32} />,
+	},
+	{
+		name: "MY_PAGE",
+		path: "/my-page",
+		text: "마이 페이지",
+		icon: <Smiley size={32} />,
+	},
+];
+
 function Footer() {
 	return (
 		<FooterContainer>
 			<NavigationBar>
-				<NavigationItem>
-					<Alarm size={28} />
-					<NavigationText>데일리 미션</NavigationText>
-				</NavigationItem>
-				<NavigationItem>
-					<CalendarCheck size={28} />
-					<NavigationText>미션 현황</NavigationText>
-				</NavigationItem>
-				<NavigationItem>
-					<Smiley size={28} />
-					<NavigationText>마이 페이지</NavigationText>
-				</NavigationItem>
+				{NavLinks.map((navLink) => (
+					<Link href={navLink.path} key={navLink.name}>
+						<NavigationItem>
+							{navLink.icon}
+							<NavigationText>{navLink.text}</NavigationText>
+						</NavigationItem>
+					</Link>
+				))}
 			</NavigationBar>
 		</FooterContainer>
 	);
