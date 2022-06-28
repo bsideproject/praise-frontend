c239f7eeeb4648de8e1063c1fd4003e83bda5840
toycrane <alwaysfun2183@gmail.com>
2022-06-28 15:44:38 +0900

    feature: AvatarSection 추가

--- pages/my-page.tsx
+++ pages/my-page.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
 import { ShareNetwork } from "phosphor-react";
 import styled from "styled-components";
 import { Header, StickyHeader } from "../src/components/header";
 import DefaultLayout from "../src/layouts";
+import AvatarImage from "../public/avatar.svg";
 
 const Title = styled.div`
 	font-size: 24px;
 	line-height: 150%;
+	font-weight: 500;
+`;
+
+const AvatarSection = styled.div`
+	padding-top: 24px;
+	padding-bottom: 24px;
+	display: flex;
+	column-gap: 13px;
+	align-items: center;
+	border-bottom: 1px ${({ theme }) => theme.colors.gray10} solid;
+`;
+
+const AvatarName = styled.div`
+	font-size: 20px;
 	font-weight: 500;
 `;
 
@@ -18,6 +34,10 @@ function MyPage() {
 					<ShareNetwork size={28} />
 				</Header>
 			</StickyHeader>
+			<AvatarSection>
+				<Image src={AvatarImage} alt="avatar Image" />
+				<AvatarName>김지구</AvatarName>
+			</AvatarSection>
 			Hello
 		</DefaultLayout>
 	);
