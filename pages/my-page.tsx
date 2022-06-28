5b572656ad529fdfe2afdcc42ee5c4ea4aa6f31b
toycrane <alwaysfun2183@gmail.com>
2022-06-28 16:49:47 +0900

    feature: MissionStatusSection 구현

--- pages/my-page.tsx
+++ pages/my-page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ShareNetwork } from "phosphor-react";
+import { Calendar, ShareNetwork } from "phosphor-react";
 import styled from "styled-components";
 import { Header, StickyHeader } from "../src/components/header";
 import DefaultLayout from "../src/layouts";
@@ -25,6 +25,59 @@ const AvatarName = styled.div`
 	font-weight: 500;
 `;
 
+const StatusSection = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const MissionCardContainer = styled.div`
+	display: flex;
+	column-gap: 6px;
+`;
+
+const MissionStatusSection = styled.div`
+	padding: 24px 16px 24px 16px;
+	display: flex;
+	row-gap: 16px;
+	flex-direction: column;
+	border-bottom: 1px ${({ theme }) => theme.colors.gray10} solid;
+`;
+
+const MissionCard = styled.div`
+	border: 1px dashed ${({ theme }) => theme.colors.gray2};
+	border-radius: 10px;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	row-gap: 6px;
+	padding: 12px 0px 8px 0px;
+`;
+
+const SectionTitle = styled.div`
+	font-size: 14px;
+	line-height: 21px;
+	font-weight: 500;
+`;
+
+const Text = styled.div`
+	font-size: 12px;
+`;
+
+const StrongText = styled.span`
+	font-size: 18px;
+	line-height: 27px;
+	font-weight: 700;
+`;
+
+const TextContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
 function MyPage() {
 	return (
 		<DefaultLayout>
@@ -38,6 +91,40 @@ function MyPage() {
 				<Image src={AvatarImage} alt="avatar Image" />
 				<AvatarName>김지구</AvatarName>
 			</AvatarSection>
+			<StatusSection>
+				<MissionStatusSection>
+					<SectionTitle>미션 현황</SectionTitle>
+					<MissionCardContainer>
+						<MissionCard>
+							<Calendar size={32} />
+							<TextContainer>
+								<Text>총 미션</Text>
+								<Text>
+									<StrongText>60</StrongText>개
+								</Text>
+							</TextContainer>
+						</MissionCard>
+						<MissionCard>
+							<Calendar size={32} />
+							<TextContainer>
+								<Text>완료 미션</Text>
+								<Text>
+									<StrongText>24</StrongText>개
+								</Text>
+							</TextContainer>
+						</MissionCard>
+						<MissionCard>
+							<Calendar size={32} />
+							<TextContainer>
+								<Text>미완료 미션</Text>
+								<Text>
+									<StrongText>36</StrongText>개
+								</Text>
+							</TextContainer>
+						</MissionCard>
+					</MissionCardContainer>
+				</MissionStatusSection>
+			</StatusSection>
 			Hello
 		</DefaultLayout>
 	);
