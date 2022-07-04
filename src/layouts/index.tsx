3cffbea019a3ffe9e5ba09739447d8e690c5c6c2
toycrane <alwaysfun2183@gmail.com>
2022-07-05 00:23:39 +0900

    fix: modal layout을 위해 default layout 수정

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,16 +2,9 @@ import styled from "styled-components";
 import Footer from "../components/footer";
 
 const PageContainer = styled.div`
-	padding-left: 16px;
-	padding-right: 16px;
 	display: flex;
 	flex-direction: column;
 	min-height: 100vh;
-	background-color: ${(props) => props.theme.colors.background};
-	@media screen and (min-width: 768px) {
-		margin: 0 auto;
-		width: 650px;
-	}
 `;
 
 const Content = styled.div`
